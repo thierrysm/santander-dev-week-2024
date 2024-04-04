5a14bb2441094ccd60407960a57f1e76fc1af489
thierry <thierrydev48@gmail.com>
2024-04-04 16:44:03 -0300

    feat: create ChampionResource

---
+++ src/main/java/com/tsm/santanderdevweek2024/controllers/ChampionResource.java
@@ -0,0 +1,25 @@
+package com.tsm.santanderdevweek2024.controllers;
+
+import com.tsm.santanderdevweek2024.domain.entities.Champion;
+import com.tsm.santanderdevweek2024.services.impl.ChampionServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/champions")
+@AllArgsConstructor
+public class ChampionResource {
+
+    private ChampionServiceImpl championService;
+
+    @GetMapping
+    public ResponseEntity<List<Champion>> findAll() {
+        var list = championService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
